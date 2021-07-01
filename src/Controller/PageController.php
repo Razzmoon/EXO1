3f97867f4fb7d935f548c309778e7b40bf8d4a67
johanrobert <robertjohan0@gmail.com>
2021-07-01 15:40:09 +0200

    Redirection

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,8 +1,9 @@
-
 <?php
 
 namespace App\Controller;
 
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,18 +12,37 @@ class PageController
     /**
      * @Route("/", name="home")
      */
-    public function home(){
-
-        // Je retourne une réponse HTTP valide en utilisant
-        // la class Response du composant HttpFoundation
+    public function home()
+    {
+        // je retourne une réponse HTTP valide en utilisant
+        // la classe Response du composant HTTPFoundation
         return new Response('Accueil');
     }
 
     /**
-     * @Route("/legal", name="legal")
+     * @Route("/contact", name="contact")
      */
-    public function legal(){
-        return new Response('Mentions légales');
+    public function contact()
+    {
+
+        // j'utilise la classe Request du composant HTTPFoundation
+        // et la méthode createFromGlobals qui met permet de récupérer
+        // tous les parametre GET / POST etc
+        $request = Request::createFromGlobals();
+
+        // je stocke dans une variable $request la valeur
+        // du parametre GET 'sent'
+        $sent = $request->query->get('sent');
+
+        // si le parametre GET 'sent' est égal à 'yes' alors j'envoie
+        // une réponse avec 'merci pour le form'
+        if ($sent === 'yes') {
+            return new Response("merci pour le formulaire");
+            // sinon je renvoie le formulaire en réponse
+        } else {
+            return new Response("formulaire");
+        }
+
     }
 
 }
--- src/Controller/pokercontroller.php
+++ src/Controller/pokercontroller.php
@@ -1,28 +1,44 @@
-
 <?php
 
 namespace App\Controller;
 
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-class PageController
+class pokercontroller extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/poker", name="poker")
      */
-    public function home(){
+    public function poker()
+    {
 
-        // Je retourne une réponse HTTP valide en utilisant
-        // la class Response du composant HttpFoundation
-        return new Response('Accueil');
+        // j'utilise la classe Request du composant HTTPFoundation
+        // et la méthode createFromGlobals qui met permet de récupérer
+        // tous les parametre GET / POST etc
+        $request = Request::createFromGlobals();
+
+        // je stocke dans une variable $request la valeur
+        // du parametre GET 'sent'
+        $age = $request->query->get('age');
+
+        // si le parametre GET 'sent' est égal à 'yes' alors j'envoie
+        // une réponse avec 'merci pour le form'
+        if ($age >= 18) {
+            return new Response("Bienvenue");
+            // sinon je renvoie le formulaire en réponse
+        } else {
+            return $this->redirectToRoute('Gros Noob');
+        }
     }
 
     /**
-     * @Route("/legal", name="legal")
+     * @Route("/Gros Noob", name="Gros Noob")
      */
-    public function legal(){
-        return new Response('Mentions légales');
+    public function Noob()
+    {
+        return new Response("Va a la maternelle");
     }
-
 }
