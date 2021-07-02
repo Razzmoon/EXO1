73b3bd7596c145a054a4f6398c414d2af81e5127
johanrobert <robertjohan0@gmail.com>
2021-07-02 16:42:33 +0200

    articles twig avec css

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,65 +1,65 @@
 <?php
 
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use  Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
 
+    private $articles = [
+        1 => [
+            "title" => "La vaccination c'est trop géniale",
+            "content" => "bablablblalba",
+            "id" => 1
+        ],
+        2 => [
+            "title" => "La vaccination c'est pas trop géniale",
+            "content" => "blablablabla",
+            "id" => 2
+        ],
+        3 => [
+            "title" => "Balkany c'est trop génial",
+            "content" => "balblalblalb",
+            "id" => 3
+        ],
+        4 => [
+            "title" => "Balkany c'est pas trop génial",
+            "content" => "balblalblalb",
+            "id" => 4
+        ]
+    ];
+
 
     /**
-     * @Route ("/articles", name="articleList")
+     * @Route("/articles", name="articleList")
      */
-    public function articleList ()
+    public function articleList()
     {
-        return new Response('articles');
-    }
-
-
-    /**
-     * j'utilise le système de wildcard de Symfony
-     * pour déclarer une URL avec une "variable" "id"
-     * qui se situe après "/article/"
-     * Pour récupérer la valeur de cette variable (wildcard)
-     * j'ai simplement a passer en parametre de méthode
-     * une variable qui a le même nom que la wildcard
-     * @Route ("/article/{id}", name="articleShow")
-     */
-    public function articleShow ($id)
-    {
-
-        $articles = [
-            1 => [
-                "title" => "La vaccination c'est trop géniale",
-                "content" => "Vaccin",
-                "id" => 1
-            ],
-            2 => [
-                "title" => "La vaccination c'est pas trop géniale",
-                "content" => "Naccin",
-                "id" => 2
-            ],
-            3 => [
-                "title" => "Balkany c'est trop génial",
-                "content" => "Paccin",
-                "id" => 3
-            ],
-            4 => [
-                "title" => "Balkany c'est pas trop génial",
-                "content" => "Baccin",
-                "id" => 4
-            ]
-        ];
-
-        return $this->render('article_show.html.twig', [
-            'article' => $articles[$id]
+        return $this->render('article_list.html.twig', [
+            'articles' => $this->articles
         ]);
-
     }
+
+    /**
+     * @Route("/article/{id}", name="articleShow")
+     */
+    public function articleShow($id)
+    {
+        // j'utilise la méthode render de l'AbstractController
+        // pour récupérer un fichier Twig, le transformer en HTML
+        // et le renvoyer en réponse HTTP au navigateur
+        // Pour utiliser des variables dans le fichier twig, je dois
+        // lui passer un tableau en second parametre, avec toutes les
+        // variables que je veux utiliser
+        return $this->render('article_show.html.twig', [
+            'article' => $this->articles[$id]
+        ]);
+    }
+
 }
 
 
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,46 +1,35 @@
 <?php
 
-
 namespace App\Controller;
 
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use  Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-class PageController
+class PageController extends AbstractController
 {
-
     /**
      * @Route("/", name="home")
      */
-    public function home ()
+    public function home()
     {
         // je retourne une réponse HTTP valide en utilisant
         // la classe Response du composant HTTPFoundation
         return new Response('Accueil');
     }
 
-
-    /**
-     * @Route("/legal", name="legal")
-     */
-    public function legal ()
-    {
-        return new Response('Mentions Légales');
-    }
-
-
     /**
      * @Route("/contact", name="contact")
+     *
+     * j'utilise "l'autowire" pour instancier automatiquement
+     * la classe Request dans la variable $request
+     * Pour faire ça, je mets la variable $request en param
+     * de ma méthode, précédé de la classe
      */
-    public function contact()
+    public function contact(Request $request)
     {
-
-        // j'utilise la classe Request du composant HTTPFoundation
-        // et la méthode createFromGlobals qui me permet de récupérer
-        // tous les parametre GET / POST etc
-        $request = Request::createFromGlobals();
-
         // je stocke dans une variable $request la valeur
         // du parametre GET 'sent'
         $sent = $request->query->get('sent');
@@ -55,4 +44,38 @@ class PageController
         }
 
     }
+
+    /**
+     * @Route("/poker", name="poker")
+     * J'utilise l'autowire de Symfony pour instancier automatiquement
+     * la classe Request dans une variable $request
+     * Ce méchanisme est aussi appelé "Injection de dépendances"
+     */
+    public function poker(Request $request)
+    {
+        $age = $request->query->get('age');
+
+        if ($age < 18 || is_null($age)) {
+            // je fais une redirection vers une autre page
+            // en utilisant la méthode redirectToRoute() récupéré
+            // depuis l'héritage d'AbstractController
+
+//           $url = $this->generateUrl("digimon");
+//           return $this->redirect($url);
+            return $this->redirectToRoute("digimon");
+        } else if ($age >= 18 && $age < 70) {
+            return new Response('Poker blablab');
+        } else {
+            return new Response('Retourne jouer à la crapette');
+        }
+    }
+
+    /**
+     * @Route("/digimon", name="digimon")
+     */
+    public function digimon()
+    {
+        return new Response('digimon');
+    }
+
 }
