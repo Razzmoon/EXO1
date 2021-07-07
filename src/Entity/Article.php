a08ec3aeb46e447fed5e56f45ece08d21e5d8cdb
johanrobert <robertjohan0@gmail.com>
2021-07-07 11:03:30 +0200

    maj

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -1,11 +1,15 @@
 <?php
+
+
 namespace App\Entity;
+
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity()
  */
-class Article{
+class Article
+{
     /**
      * @ORM\Id()
      * @ORM\Column(type="integer")
@@ -19,13 +23,88 @@ class Article{
     private $title;
 
     /**
-     * @ORM\Column (type="string")
+     * @ORM\Column(type="text")
      */
     private $content;
 
     /**
-     * @ORM\Column
+     * @ORM\Column(type="datetime")
      */
-    private $createdAT;
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isPublished;
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(?bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+
 
 }
